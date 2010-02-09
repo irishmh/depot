8cf0d3f44f91b51facc7f18fa75928ab7a3f3287
unknown <owner@.(none)>
2010-02-09 17:01:24 -0600

    changes re Task E & F

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
-  
+  has_many :line_items
+      
   def self.find_products_for_sale
     find(:all, :order => "title" )
   end
-  
+
   validates_presence_of :title, :description, :image_url
   validates_numericality_of :price
   validate :price_must_be_at_least_a_cent
